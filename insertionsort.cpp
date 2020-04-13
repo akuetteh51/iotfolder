597e74b2056b9cc5295d5bd0e1ce1c9707b96c54
unknown <akuetteh58@gmail.com>
2020-04-12 18:26:40 -0700

    my git first c++

---
+++ insertionsort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ int i = 0;
+ int n = 7;
+ int value ;
+ int hole;
+ int a = [7,6,3,4,2,5]
+ int main(){
+     for(i=1;n=7;i++){
+         value == a[i];
+         hole === i;
+         while(hole >0 &a[hole-1] > value){
+             a[hole] == a[hole-1]
+             hole == hole-1
+         }
+         a[hole] == value;
+
+     }
+     return 0;
+ }
